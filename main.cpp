bb836f91e0dbc6d7c007b6afb32211593d3fd472
Shubham2624 <70702649+Shubham26997@users.noreply.github.com>
2022-05-19 13:50:14 +0000

    Arrays pointers

--- main.cpp
+++ main.cpp
@@ -24,19 +24,19 @@ int main() {
   /*
   Dangling pointers are need to be avoided as this are very dangerous why
   because they point to the address automatically and this may result to a very
-  large conflict.
+  large conflict. This also mean that it point to a memory location which is deallocated. 
 */
-  cout << "Starting with dangling!!" << endl;
-  int *dangling;
-  // this is a dangling pointer which is not pointing to any object according to
-  // me but internally it may point to some specific random address.
-  cout << dangling << endl; // Print value stored in danling.
-  cout << *dangling
-       << endl; // Print value stored at the address stored in dangling
-  cout << &dangling << endl; // Print address of dangling
-  cout << "That's it for dangling so don't use this as this is not a good "
-          "practice!! "
-       << endl;
+  // cout << "Starting with dangling!!" << endl;
+  // int *dangling;
+  // // this is a dangling pointer which is not pointing to any object according to
+  // // me but internally it may point to some specific random address.
+  // cout << dangling << endl; // Print value stored in danling.
+  // cout << *dangling
+  //      << endl; // Print value stored at the address stored in dangling
+  // cout << &dangling << endl; // Print address of dangling
+  // cout << "That's it for dangling so don't use this as this is not a good "
+  //         "practice!! "
+  //      << endl;
 
   // If you need to use null pointer you can proceed like this
   int s = 26;   // created a variable which need to  be used.
@@ -44,5 +44,22 @@ int main() {
   ptr = &s;     // now pointer is storing the address of s.
 
   cout << "Starting with pointers in array!!" << endl;
+    //In array for C++ the first index is the base address of the array and each array is at the contiguous memory location so next will be at 104 address if first is at 100 address.
+    int arr[] = {2,6,84,21,4};
+
+    // Both produce the same address as this is called self-referencing
+    cout << arr << endl; //Print the address of the array arr.
+    cout << &arr << endl; // Print the address of the array arr.
+/*
+    arr = pointer to the first element
+    &arr = pointer to the whole array if int arr[5] so this will be pointing to the whole array of size 5 
+    So, when we print arr and &arr they both point to same address location since array is at continous memory location so they both point to same address hence printed the same address.
+*/
+    // cout << arr+1 << endl;
+    // cout << &arr+1 << endl;
+    cout << *arr << endl; // Print value at the address of arr[0] since in case of arrays *arr == *(arr+0)
+
+    cout << 2[arr] << endl; // Since the array is a pointer so *(arr+i) == *(i+arr) where i is index of the array so this mean we can access any element of the array with index[arr] too in c++.
+
     
 }
